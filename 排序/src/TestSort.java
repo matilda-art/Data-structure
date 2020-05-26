1a5ada9f5aea9f2c9347aeb68fa564ea906d3dda
matilda-art <heysober@163.com>
2020-05-26 17:30:21 +0800

    更新

--- 排序/src/TestSort.java
+++ 排序/src/TestSort.java
@@ -304,9 +304,58 @@ public class TestSort {
         }
     }
 
-    public static void mergeSort(int[] array){
+    public static void mergeSort1(int[] array){
         mergeSortInternal(array,0,array.length-1);
     }
+
+
+    //归并排序（非递归）
+    public static void mergeSort(int[] array) {
+        for (int gap = 1; gap < array.length; gap *= 2) {
+            mergeNor(array,gap);
+        }
+    }
+
+    public static void mergeNor(int[] array,int gap) {
+        int[] ret = new int[array.length];
+        int k = 0;//ret的下标
+        int s1 = 0;
+        int e1 = s1+gap-1;
+        int s2 = e1+1;
+        int e2 = s2+gap-1 < array.length ? s2+gap-1 :  array.length-1;
+
+        //1、肯定是有两个归并段的
+        while (s2 < array.length) {
+            //2、对应的s1位置和s2位置进行比较
+            while (s1 <= e1 && s2 <= e2){
+                if (array[s1] < array[s2]){
+                    ret[k++] = array[s1++];
+                }else {
+                    ret[k++] = array[s2++];
+                }
+            }
+            //3、上述第2步在比较的过程当中，肯定会有一个下标先走完一个归并段
+            //4、判断是谁没走完，把剩下的数据拷贝到结果数组当中
+            while (s1 <= e1){
+                ret[k++] = array[s1++];
+            }
+            while (s2 <= e2){
+                ret[k++] = array[s2++];
+            }
+            //5、接着确定新的s1,e1,s2,e2
+            s1 = s2;
+            e1 = s1+gap-1 < array.length ? s1+gap-1 : array.length-1;
+            s2 = e1+1;
+            e2 = s2+gap-1 < array.length ? s2+gap-1 : array.length-1;
+        }
+        while (s1 <= array.length-1){
+            ret[k++] = array[s1++];
+        }
+        for (int i = 0; i < ret.length; i++) {
+            array[i] = ret[i];
+        }
+    }
+
 
     public static void main1(String[] args) {
         int[] array = new int[10_0000];
