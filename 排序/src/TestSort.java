a84ebd08b4f11440a5a2b6fc20b25638be5316b6
小楫轻舟 <62049028+matilda-art@users.noreply.github.com>
2020-09-02 17:25:35 +0800

    七大排序
    七大排序

--- 排序/src/TestSort.java
+++ 排序/src/TestSort.java
@@ -80,7 +80,7 @@ public class TestSort {
     //选择排序
     public static void selectSort(int[] array) {
         for (int i = 0; i < array.length; i++) {
-            for (int j = i; j < array.length; j++) {
+            for (int j = i+1; j < array.length; j++) {
                 if (array[i] > array[j]){
                     int tmp = array[i];
                     array[i] = array[j];
